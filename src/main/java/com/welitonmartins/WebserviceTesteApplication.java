d7ed48720a330ea290938349420e423cf9b0208d
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-08-13 20:22:32 -0300

    primeiro commit

---
+++ src/main/java/com/welitonmartins/WebserviceTesteApplication.java
@@ -0,0 +1,12 @@
+package com.welitonmartins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceTesteApplication.class, args);
+	}
+}
