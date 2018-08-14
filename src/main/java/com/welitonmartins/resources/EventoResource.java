92c9f01ee03de60b912b49e2710af274d9543fd5
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-08-13 21:00:06 -0300

    criando post, delete

--- src/main/java/com/welitonmartins/resources/EventoResource.java
+++ src/main/java/com/welitonmartins/resources/EventoResource.java
@@ -1,7 +1,12 @@
 package com.welitonmartins.resources;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +22,19 @@ public class EventoResource {
 	private EventoRepository er;
 	
 	@GetMapping(produces="application/json")
-	public @ResponseBody void listaEventos() {
+	public @ResponseBody Iterable<Evento> listaEventos() {
 		Iterable<Evento> listaEventos = er.findAll();
 		return listaEventos;
 	}
 	
+	@PostMapping
+	public Evento cadastroEvento(@RequestBody @Valid Evento evento) {
+		return er.save(evento);
+	}
+	
+	@DeleteMapping
+	public Evento deletaEvento(@RequestBody Evento evento) {
+		er.delete(evento);
+		return evento; 
+	}
 }
